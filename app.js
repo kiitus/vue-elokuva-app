985692d59e38a665ba06689c96cd4677a20662cb
teemu <teemu.hallinen@hotmail.com>
2021-01-20 23:25:06 +0200

    redirect_main

--- app.js
+++ app.js
@@ -2,4 +2,8 @@ const express = require('express');
 const port = process.env.PORT || 8080;
 const app = express();
 app.use(express.static('Bootstrap'))
+app.get("/",(req,res)=>
+{
+    res.redirect("/main.html")
+})
 app.listen(port);
